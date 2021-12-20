bdeaa6217b132d4b2cd8815a37903ab8be02fdb0
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-12-20 15:18:03 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'styles/App.scss';
-import { routes } from 'routes';
-import { RecoilRoot } from 'recoil';
-import Toaster from 'components/Toaster';
+import "styles/App.scss";
+import { routes } from "routes";
+import { RecoilRoot } from "recoil";
+import Toaster from "components/Toaster";
 
 function App() {
   return (
     <RecoilRoot>
       <Toaster />
       <Routes>
-        {
-          routes.map(([path, Element], i) =>
-            <Route path={path} element={<Element />} key={i} />
-          )
-        }
+        {routes.map(([path, Element], i) => (
+          <Route path={path} element={<Element />} key={i} />
+        ))}
       </Routes>
     </RecoilRoot>
   );
--- src/components/Enter/index.tsx
+++ src/components/Enter/index.tsx
@@ -5,22 +5,26 @@ import { useRecoilState } from "recoil";
 import { nameState } from "atoms/nameState";
 
 export default function Enter() {
+  const [name, setName] = useRecoilState(nameState);
+  const navigate = useNavigate();
 
-    const [name, setName] = useRecoilState(nameState);
-    const navigate = useNavigate()
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    name && navigate("/play");
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        name && navigate("/play")
-    }
+  return (
+    <div id="enter">
+      <h1>STRIVE TIC TAC</h1>
 
-    return <div id="enter">
-        <h1>STRIVE TIC TAC</h1>
-
-        <Form onSubmit={handleSubmit}>
-            <Form.Control type="text" value={name} onChange={e => setName(e.target.value)} />
-            <Form.Control type="submit" disabled={!name} />
-        </Form>
-
+      <Form onSubmit={handleSubmit}>
+        <Form.Control
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Form.Control type="submit" disabled={!name} />
+      </Form>
     </div>
+  );
 }
